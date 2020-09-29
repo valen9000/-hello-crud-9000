0de50137445558fca15e9108556f96b506154ffd
valen9000 <67491385+valen9000@users.noreply.github.com>
2020-09-29 15:50:16 -0300

    Update note.js

--- api/routes/note.js
+++ api/routes/note.js
@@ -3,26 +3,25 @@ const router = express.Router();
 
 const Note = require('../models/Note');
 
-// POST /notes
+
 router.post('/notes', (req, res, next) => {
   const note = new Note({
-    title: req.body.title,
-    text: req.body.text
-  });
-  note.save((err, note) => {
-    if (err) return next(err);
-    res.status(201).json(note);
-  });
+      title: req.body.title,
+      text: req.body.text
+    });
+    note.save((err, note) => {
+      if (err) return next(err);
+      res.status(201).json(note);
+    });
 });
 
-// GET /notes
 router.get('/notes', (req, res, next) => {
-  Note.find()                  // todos los docs de notes
-    .select('_id title text')  // como SELECT en SQL
-    .sort('-updatedAt')        // ordena por modificacion descendente
+  Note.find()                  
+    .select('_id title text')  
+    .sort('-updatedAt')        
     .exec((err, notes) => {
       if (err) return next(err);
-      // modifico un poco el resultado antes de mandarlo
+     
       notes = notes.map(note => ({
         title: note.title,
         text: note.text,
@@ -32,9 +31,9 @@ router.get('/notes', (req, res, next) => {
         }
       }));
       res.status(200).json({
-        count: notes.length,   // la cantidad de elementos en notes
+        count: notes.length,   
         notes: notes,
-        create: {              // como crear una nota
+        create: {              
           method: 'POST',
           url: `${req.protocol}://${req.hostname}:3000/api/notes`
         }
@@ -42,10 +41,9 @@ router.get('/notes', (req, res, next) => {
     });
 });
 
-// GET /notes/id
 router.get('/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
-    .select('_id title text createdAt updatedAt')  // todo menos __v
+    .select('_id title text createdAt updatedAt')  
     .exec((err, note) => {
       if (err) return next(err);
       if (!note) return res.status(404).json({ msg: 'Not found' });
@@ -64,7 +62,7 @@ router.get('/notes/:id', (req, res, next) => {
       });
     });
 });
-// PUT /notes/id
+
 router.put('/notes/:id', (req, res, next) => {
   const note = {
     title: req.body.title,
@@ -82,7 +80,6 @@ router.put('/notes/:id', (req, res, next) => {
   });
 });
 
-// DELETE /notes/id
 router.delete('/notes/:id', (req, res, next) => {
   Note.findByIdAndRemove(req.params.id).exec((err, note) => {
     if (err) return next(err);
