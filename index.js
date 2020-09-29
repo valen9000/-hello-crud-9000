0978bd8101aa258db2a223ba20ee4732ed71615e
valen9000 <67491385+valen9000@users.noreply.github.com>
2020-09-29 14:47:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express  = require('express');
 const mongoose = require('mongoose');
 const cors     = require('cors');
 const morgan   = require('morgan');
+
+
 const port = process.env.PORT        || 3000;
 const db   = process.env.MONGODB_URI || 'mongodb://localhost/notas';
+
 const app = express();
+
+
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
 mongoose
@@ -13,19 +18,25 @@ mongoose
     console.log(`DB connected @ ${db}`);
   })
   .catch(err => console.error(`Connection error ${err}`));
+
+// todo el middleware aca abajo y antes del listen
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use('/api', require('./api/routes/note'));
+
 app.use((req, res, next) => {
   const err = new Error('Not found');
   err.status = 404;
   next(err);
 });
+
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
-    res.json({ error: err.message });
+  res.json({ error: err.message });
 });
+
+// listen
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
-});  
+});
